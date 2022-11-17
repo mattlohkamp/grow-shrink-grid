7e9b63ff0b0b8457779642ad7188c3cee9ba0a89
mattlohkamp <work@mattlohkamp.com>
2022-11-17 12:45:00 -0800

    class and constructor

---
+++ index.ts
@@ -0,0 +1,37 @@
+export enum GridError {
+	DIMENSION_OUT_OF_BOUNDS = "DIMENSION_OUT_OF_BOUNDS",
+}
+
+export class Grid {
+	entries?: unknown[];
+
+	#rowCount?: number | undefined;
+	public get rowCount(): number | undefined {
+		return this.#rowCount;
+	}
+	public set rowCount(value: number | undefined) {
+		if (value && value <= 0) {
+			console.error("rowCount may not be set to 0 or less");
+			throw new Error(GridError.DIMENSION_OUT_OF_BOUNDS);
+		}
+		this.#rowCount = value;
+	}
+
+	#columnCount?: number | undefined;
+	public get columnCount(): number | undefined {
+		return this.#columnCount;
+	}
+	public set columnCount(value: number | undefined) {
+		if (value && value <= 0) {
+			console.error("columnCount may not be set to 0 or less");
+			throw new Error(GridError.DIMENSION_OUT_OF_BOUNDS);
+		}
+		this.#columnCount = value;
+	}
+	constructor(_entries: unknown[] = [], _rowCount = 1, _columnCount = 1) {
+		this.entries = _entries;
+		this.rowCount = _rowCount;
+		this.columnCount = _columnCount;
+	}
+}
+export default Grid;
