3d42dda2c7221ff670a88a2187b057eb8909d17d
mattlohkamp <work@mattlohkamp.com>
2022-11-18 13:40:01 -0800

    more required values, better error catching tests

--- index.test.ts
+++ index.test.ts
@@ -1,60 +1,94 @@
-import Grid, { GridError, isDefaultGrid } from ".";
+import Grid, { GridError, isDefaultGrid } from '.';
 
-describe("The Grid object", () => {
-	it("throws an error on invalid arguments passed to constructor", () => {
-		try {
-			new Grid([], 0);
-			new Grid([], 1, 0);
-			new Grid([], 0, 0);
-		} catch (error) {
-			expect(error).toThrow(GridError.DIMENSION_OUT_OF_BOUNDS);
-		}
-	});
-	it("instanciates with default values", () => {
-		const testGrid1 = new Grid();
-		expect(isDefaultGrid(testGrid1)).toBe(true);
-		const testGrid2 = new Grid([]);
-		expect(isDefaultGrid(testGrid2)).toBe(true);
-		const testGrid3 = new Grid([], 1);
-		expect(isDefaultGrid(testGrid3)).toBe(true);
-		const testGrid4 = new Grid([], 1, 1);
-		expect(isDefaultGrid(testGrid4)).toBe(true);
-	});
-	it("initializes properties from arguments passed to constructor", () => {
-		const entriesArg = ["test"];
-		const rowCountArg = 2;
-		const columnCountArg = 3;
-		const testGrid = new Grid(entriesArg, rowCountArg, columnCountArg);
-		expect(testGrid.entries).toBe(entriesArg);
-		expect(testGrid.rowCount).toBe(rowCountArg);
-		expect(testGrid.columnCount).toBe(columnCountArg);
-	});
-	it("allows access to members by index", () => {
-		const entriesArg = ["a1", "a2", "b1", "b2"];
-		/*
+describe('The Grid object', () => {
+  it('throws an error on invalid arguments passed to constructor', () => {
+    //	_rowCount and _columnCount args must be at least 1
+    expect(() => {
+      new Grid([], 0);
+    }).toThrow(GridError.DIMENSION_OUT_OF_BOUNDS);
+    expect(() => {
+      new Grid([], 1, 0);
+    }).toThrow(GridError.DIMENSION_OUT_OF_BOUNDS);
+    expect(() => {
+      new Grid([], 0, 0);
+    }).toThrow(GridError.DIMENSION_OUT_OF_BOUNDS);
+  });
+  it('instanciates with default values', () => {
+    const testGrid1 = new Grid();
+    expect(isDefaultGrid(testGrid1)).toBe(true);
+    const testGrid2 = new Grid([]);
+    expect(isDefaultGrid(testGrid2)).toBe(true);
+    const testGrid3 = new Grid([], 1);
+    expect(isDefaultGrid(testGrid3)).toBe(true);
+    const testGrid4 = new Grid([], 1, 1);
+    expect(isDefaultGrid(testGrid4)).toBe(true);
+  });
+  it('initializes properties from arguments passed to constructor', () => {
+    const entriesArg = ['test'];
+    const rowCountArg = 2;
+    const columnCountArg = 3;
+    const testGrid = new Grid(entriesArg, rowCountArg, columnCountArg);
+    expect(testGrid.entries).toBe(entriesArg);
+    expect(testGrid.rowCount).toBe(rowCountArg);
+    expect(testGrid.columnCount).toBe(columnCountArg);
+  });
+  it('requires valid dimensions', () => {
+    const testGrid = new Grid();
+    //	dimensions must be greater than zero
+    expect(() => {
+      testGrid.rowCount = 0;
+    }).toThrow(GridError.DIMENSION_OUT_OF_BOUNDS);
+    expect(() => {
+      testGrid.rowCount = -1;
+    }).toThrow(GridError.DIMENSION_OUT_OF_BOUNDS);
+    expect(() => {
+      testGrid.columnCount = 0;
+    }).toThrow(GridError.DIMENSION_OUT_OF_BOUNDS);
+    expect(() => {
+      testGrid.columnCount = -1;
+    }).toThrow(GridError.DIMENSION_OUT_OF_BOUNDS);
+  });
+  it('allows access to members by index', () => {
+    const entriesArg = ['a1', 'a2', 'b1', 'b2'];
+    /*
 				A1 | A2
 				———————
 				B2 | B2
 		*/
-		const testGrid = new Grid(entriesArg, 2, 2);
-		const testIndex = 2; //	third entry - e.g. B2
-		expect(testGrid.entries?.[testIndex]).toBe(entriesArg[testIndex]);
-		expect(testGrid.getCellByIndex(testIndex)).toBe(entriesArg[testIndex]);
-	});
-	it("allows access to members by coordinates", () => {
-		const entriesArg = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"];
-		/*
+    const testGrid = new Grid(entriesArg, 2, 2);
+    const testIndex = 2; //	third entry - e.g. B2
+    expect(testGrid.entries?.[testIndex]).toBe(entriesArg[testIndex]);
+    expect(testGrid.getCellByIndex(testIndex)).toBe(entriesArg[testIndex]);
+  });
+  it('allows access to members by coordinates', () => {
+    const entriesArg = ['a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2', 'c3'];
+    /*
 				A1 | A2 | A3
 				————————————
 				B1 | B2 | B3
 				————————————
 				C1 | C2 | C3
 		*/
-		const testGrid = new Grid(entriesArg, 3, 3);
-		const [testCoordsX, testCoordsY] = [1, 2]; //	second column, third row - e.g. C2
-		//	TODO: add tests for invalid coords
-		expect(testGrid.getCellByCoordinates(testCoordsX, testCoordsY)).toBe(
-			entriesArg[7]
-		);
-	});
+    const testGrid = new Grid(entriesArg, 3, 3);
+    //	second column, third row - e.g. C2
+    expect(testGrid.getCellByCoordinates(1, 2)).toBe(entriesArg[7]);
+  });
+  it('errors out on invalid coordinates', () => {
+    //	prettier-ignore
+    const entriesArg = ['a1', 'a2', 'a3', 'a4', 'b1', 'b2', 'b3', 'b4', 'c1', 'c2', 'c3', 'c4'];
+    /*
+				A1 | A2 | A3 | A4
+				—————————————————
+				B1 | B2 | B3 | B4
+				—————————————————
+				C1 | C2 | C3 | C4
+		*/
+    const testGrid = new Grid(entriesArg, 3, 4);
+    expect(() => testGrid.getCellByCoordinates(5, 0)).toThrow(
+      GridError.COORDINATE_OUT_OF_BOUNDS
+    );
+    expect(() => testGrid.getCellByCoordinates(-1, 0)).toThrow(
+      GridError.COORDINATE_OUT_OF_BOUNDS
+    );
+  });
 });
--- index.ts
+++ index.ts
@@ -1,106 +1,105 @@
+// TODO: unify terminology for cols and x and rows and y
+
 export enum GridError {
-	DIMENSION_OUT_OF_BOUNDS = "OUT_OF_BOUNDS",
-	COORDINATE_OUT_OF_BOUNDS = "COORDINATE_OUT_OF_BOUNDS",
-	EMPTY_ENTRIES = "EMPTY_ENTRIES",
+  DIMENSION_OUT_OF_BOUNDS = 'OUT_OF_BOUNDS',
+  COORDINATE_OUT_OF_BOUNDS = 'COORDINATE_OUT_OF_BOUNDS',
+  EMPTY_ENTRIES = 'EMPTY_ENTRIES',
 }
 
 const ErrorMessages = {
-	[GridError.DIMENSION_OUT_OF_BOUNDS]: (name: string) =>
-		`${name} may not be set to 0 or less`,
-	[GridError.COORDINATE_OUT_OF_BOUNDS]: (
-		name: string,
-		value: number,
-		max: number
-	) =>
-		`${name} value "${value}" out of bounds, expected a value between 0 and ${max}`,
-	[GridError.EMPTY_ENTRIES]: (index: number) =>
-		`failed to access entry at index "${index}", because entries collection is empty!`,
+  [GridError.DIMENSION_OUT_OF_BOUNDS]: (name: string) =>
+    `${name} may not be set to 0 or less`,
+  [GridError.COORDINATE_OUT_OF_BOUNDS]: (
+    name: string,
+    value: number,
+    max: number
+  ) =>
+    `${name} value "${value}" out of bounds, expected a value between 0 and ${max}`,
+  [GridError.EMPTY_ENTRIES]: (index: number) =>
+    `failed to access entry at index "${index}", because entries collection is empty!`,
 };
 
 export const DEFAULT_ROW_COUNT = 1;
 export const DEFAULT_COLUMN_COUNT = 1;
 
+export const isDefaultGrid = (grid: Grid<unknown>): boolean =>
+  grid.entries.length === 0 &&
+  grid.rowCount === DEFAULT_ROW_COUNT &&
+  grid.columnCount === DEFAULT_COLUMN_COUNT;
+
 export class Grid<CellDataType = void> {
-	entries?: CellDataType[];
+  entries: CellDataType[];
 
-	#rowCount?: number | undefined;
-	public get rowCount(): number | undefined {
-		return this.#rowCount;
-	}
-	public set rowCount(value: number | undefined) {
-		if (value && value <= 0) {
-			console.error(
-				ErrorMessages[GridError.DIMENSION_OUT_OF_BOUNDS]("rowCount")
-			);
-			throw new Error(GridError.DIMENSION_OUT_OF_BOUNDS);
-		}
-		this.#rowCount = value;
-	}
+  #rowCount: number;
+  get rowCount(): number {
+    return this.#rowCount;
+  }
+  set rowCount(value: number) {
+    if (value <= 0) {
+      console.error(
+        ErrorMessages[GridError.DIMENSION_OUT_OF_BOUNDS]('rowCount')
+      );
+      throw new Error(GridError.DIMENSION_OUT_OF_BOUNDS);
+    }
+    this.#rowCount = value;
+  }
 
-	#columnCount?: number | undefined;
-	public get columnCount(): number | undefined {
-		return this.#columnCount;
-	}
-	public set columnCount(value: number | undefined) {
-		if (value && value <= 0) {
-			console.error(
-				ErrorMessages[GridError.DIMENSION_OUT_OF_BOUNDS]("columnCount")
-			);
-			throw new Error(GridError.DIMENSION_OUT_OF_BOUNDS);
-		}
-		this.#columnCount = value;
-	}
+  #columnCount: number;
+  get columnCount(): number {
+    return this.#columnCount;
+  }
+  set columnCount(value: number) {
+    if (value <= 0) {
+      console.error(
+        ErrorMessages[GridError.DIMENSION_OUT_OF_BOUNDS]('columnCount')
+      );
+      throw new Error(GridError.DIMENSION_OUT_OF_BOUNDS);
+    }
+    this.#columnCount = value;
+  }
 
-	static getIndexFromXYCoordinates = (x: number, y: number, xMax: number) =>
-		// prettier-ignore
-		(y * xMax) + x;
+  static getIndexFromXYCoordinates = (x: number, y: number, xMax: number) =>
+    // prettier-ignore
+    (y * xMax) + x;
 
-	constructor(
-		_entries: CellDataType[] = [],
-		_rowCount = DEFAULT_ROW_COUNT,
-		_columnCount = DEFAULT_COLUMN_COUNT
-	) {
-		this.entries = _entries;
-		this.rowCount = _rowCount;
-		this.columnCount = _columnCount;
-	}
-	getCellByIndex(index: number): CellDataType | undefined {
-		if (this.entries?.length === 0) {
-			console.warn(ErrorMessages[GridError.EMPTY_ENTRIES]);
-			//	throw new Error(GridError.EMPTY_ENTRIES);
-		}
-		return this.entries?.[index];
-	}
-	getCellByCoordinates(x: number, y: number): CellDataType | undefined {
-		if (!this.#rowCount || !this.#columnCount) {
-			return;
-		}
-		if (x < 0 || x >= this.#rowCount) {
-			console.error(
-				ErrorMessages[GridError.COORDINATE_OUT_OF_BOUNDS](
-					"x",
-					x,
-					this.#rowCount
-				)
-			);
-			throw new Error(GridError.COORDINATE_OUT_OF_BOUNDS);
-		} else if (y < 0 || y >= this.#columnCount) {
-			console.error(
-				ErrorMessages[GridError.COORDINATE_OUT_OF_BOUNDS](
-					"y",
-					y,
-					this.#columnCount
-				)
-			);
-			throw new Error(GridError.COORDINATE_OUT_OF_BOUNDS);
-		}
-		const index = Grid.getIndexFromXYCoordinates(x, y, this.#rowCount);
-		return this.entries?.[index];
-	}
+  constructor(
+    _entries: CellDataType[] = [],
+    _rowCount = DEFAULT_ROW_COUNT,
+    _columnCount = DEFAULT_COLUMN_COUNT
+  ) {
+    this.entries = _entries;
+    this.rowCount = _rowCount;
+    this.columnCount = _columnCount;
+  }
+  getCellByIndex(index: number): CellDataType | undefined {
+    if (this.entries.length === 0) {
+      console.warn(ErrorMessages[GridError.EMPTY_ENTRIES]);
+      //	throw new Error(GridError.EMPTY_ENTRIES);
+    }
+    return this.entries?.[index];
+  }
+  getCellByCoordinates(x: number, y: number): CellDataType | undefined {
+    if (x < 0 || x >= this.#rowCount) {
+      console.error(
+        ErrorMessages[GridError.COORDINATE_OUT_OF_BOUNDS](
+          'x',
+          x,
+          this.#rowCount
+        )
+      );
+      throw new Error(GridError.COORDINATE_OUT_OF_BOUNDS);
+    } else if (y < 0 || y >= this.#columnCount) {
+      console.error(
+        ErrorMessages[GridError.COORDINATE_OUT_OF_BOUNDS](
+          'y',
+          y,
+          this.#columnCount
+        )
+      );
+      throw new Error(GridError.COORDINATE_OUT_OF_BOUNDS);
+    }
+    const index = Grid.getIndexFromXYCoordinates(x, y, this.#rowCount);
+    return this.entries[index];
+  }
 }
 export default Grid;
-
-export const isDefaultGrid = (grid: Grid<unknown>): boolean =>
-	grid.entries?.length === 0 &&
-	grid.rowCount === DEFAULT_ROW_COUNT &&
-	grid.columnCount === DEFAULT_COLUMN_COUNT;
